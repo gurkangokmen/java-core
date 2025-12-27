ac79c17e6d8573e3b6bbc5ce9565033b87518307
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2025-12-27 09:36:07 +0300

    Java Lambda

--- Lambda/lambda/src/Main.java
+++ Lambda/lambda/src/Main.java
@@ -10,25 +10,32 @@ public class Main {
     public static void main(String[] args) {
 
         // Lambda expression way 1
-        /*ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         numbers.add(5);
         numbers.add(9);
         numbers.add(8);
         numbers.add(1);
 
-        numbers.forEach( (n) -> { System.out.println(n); } );*/
+        numbers.forEach((n) -> {
+            System.out.println(n);
+        });
 
 
 
-
+        /*
         // Lambda expression way 2
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         numbers.add(5);
         numbers.add(9);
         numbers.add(8);
         numbers.add(1);
-        Consumer<Integer> method = (n) -> { System.out.println(n); };
+        Consumer<Integer> method = (n) -> {
+            System.out.println(n);
+        };
         numbers.forEach(method);
+
+         */
 
     }
 }
